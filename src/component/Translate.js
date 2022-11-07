2443ea39ef0f4ed462068913179d436d49bee5e2
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-11-07 17:26:50 +0530

    wip: language translator

--- src/component/Translate.js
+++ src/component/Translate.js
@@ -1,50 +1,89 @@
-import React, { useState } from "react";
-import "./Translate.css";
-// const { Translate } = require("@google-cloud/translate");
+import React, { useEffect, useState } from 'react';
+import './Translate.css';
 
 const Translator = () => {
   const [inputText, setInputText] = useState();
-  let url = `https://translation.googleapis.com/language/translate/v2/languages?key=AIzaSyBb6DsR8FZ0xWMsqhy8Do0zynkd2PciqH8`;
-
-  fetch(url).then(res => (res.json()).then(data => console.log(data)))
+  const [translatedText, setTranslatedText] = useState();
+  const [languages, setLanguages] = useState([]);
+  const [sourceLanguage, setSourceLanguage] = useState(navigator.language);
+  const [targetLanguage, setTargetLanguage] = useState('hi');
+  const apiKey = process.env.REACT_APP_API_KEY;
+  let url = `https://translation.googleapis.com/language/translate/v2/languages?key=${apiKey}`;
+  useEffect(() => {
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        target: navigator.language,
+      }),
+    }).then((res) =>
+      res.json().then((data) => setLanguages(data.data.languages))
+    );
+  }, [url]);
   return (
     <>
       <h1>Language Translator</h1>
-      <div className="container">
-        <div className="sub-container">
-          <select className="form-select form-select-sm"></select>
+      <div className='container'>
+        <div className='sub-container'>
+          <select
+            className='form-select form-select-sm'
+            onChange={(e) => setSourceLanguage(e.target.value)}
+            value={sourceLanguage}
+          >
+            {languages.map((language) => (
+              <option key={language.language} value={language.language}>
+                {language.name}
+              </option>
+            ))}
+          </select>
           <br />
           <textarea
             rows={4}
-            className="form-control"
+            className='form-control'
             value={inputText}
             onChange={(e) => setInputText(e.target.value)}
-            placeholder="Enter your text here"
+            placeholder='Enter your text here'
           />
-          <div className="icons">
-            <i className="bi bi-clipboard"></i>
-            <i class="bi bi-volume-up"></i>
-            <i class="bi bi-x-lg"></i>
+          <div className='icons'>
+            <i className='bi bi-clipboard'></i>
+            <i className='bi bi-volume-up'></i>
+            <i className='bi bi-x-lg'></i>
           </div>
         </div>
         <div>
-          <i class="bi bi-arrow-left-right"></i>
+          <i className='bi bi-arrow-left-right'></i>
         </div>
-        <div className="sub-container">
-          <select className="form-select form-select-sm"></select>
+        <div className='sub-container'>
+          <select
+            className='form-select form-select-sm'
+            onChange={(e) => setTargetLanguage(e.target.value)}
+            value={targetLanguage}
+          >
+            {languages.map((language) => (
+              <option key={language.language} value={language.language}>
+                {language.name}
+              </option>
+            ))}
+          </select>
           <br />
           <textarea
             rows={4}
-            className="form-control"
-            placeholder="Translation"
+            className='form-control'
+            placeholder='Translation'
+            value={translatedText}
           />
-          <div className="icons">
-            <i className="bi bi-clipboard"></i>
-            <i class="bi bi-volume-up"></i>
-            <i class="bi bi-x-lg"></i>
+          <div className='icons'>
+            <i className='bi bi-clipboard'></i>
+            <i className='bi bi-volume-up'></i>
+            <i className='bi bi-x-lg'></i>
           </div>
         </div>
       </div>
+      <button type='submit' onClick={() => setTranslatedText('hello')}>
+        Translate
+      </button>
     </>
   );
 };
