bc718937501277961b04df0a84659745586a97fa
Rutvi Patel <rutvi.p@simformsolutions.com>
2022-11-09 16:24:50 +0530

    implemented debouncing, sound & copy text functionality

--- src/component/Translate.js
+++ src/component/Translate.js
@@ -1,16 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import './Translate.css';
 
 const Translator = () => {
-  const [inputText, setInputText] = useState();
-  const [translatedText, setTranslatedText] = useState();
+  const [inputText, setInputText] = useState('');
+  const [translatedText, setTranslatedText] = useState('');
   const [languages, setLanguages] = useState([]);
   const [sourceLanguage, setSourceLanguage] = useState(navigator.language);
   const [targetLanguage, setTargetLanguage] = useState('hi');
   const apiKey = process.env.REACT_APP_API_KEY;
-  let url = `https://translation.googleapis.com/language/translate/v2/languages?key=${apiKey}`;
+  let url = `https://translation.googleapis.com/language/translate/v2/`;
   useEffect(() => {
-    fetch(url, {
+    fetch(url + `languages?key=${apiKey}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -21,17 +21,18 @@ const Translator = () => {
     }).then((res) =>
       res.json().then((data) => setLanguages(data.data.languages))
     );
-  }, [url]);
-  let url1 = `https://translation.googleapis.com/language/translate/v2/?key=${apiKey}`;
-  const translate = () => {
-    fetch(url1, {
+  }, [url, apiKey]);
+  // translate functionality
+  const translate = (text, lang) => {
+    console.log('translating...');
+    fetch(url + `?key=${apiKey}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        q: inputText,
-        target: targetLanguage,
+        q: text,
+        target: lang,
       }),
     }).then((res) =>
       res.json().then((data) => {
@@ -39,6 +40,35 @@ const Translator = () => {
         setSourceLanguage(data.data.translations[0].detectedSourceLanguage);
       })
     );
+  };
+  // sound functionality
+  const sound = (text) => {
+    console.log('sound', text);
+    const utterance = new SpeechSynthesisUtterance(text);
+    speechSynthesis.speak(utterance);
+  };
+  // debounce function
+  const debounce = (func) => {
+    let timer;
+    return function (...args) {
+      const context = this;
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(() => {
+        timer = null;
+        func.apply(context, args);
+      }, 500);
+    };
+  };
+  const optimizedFn = useCallback(debounce(translate), []);
+  // exchange function
+  const exchange = () => {
+    const tempText = inputText;
+    setInputText(translatedText);
+    setTranslatedText(tempText);
+    const tempLanguage = sourceLanguage;
+    setSourceLanguage(targetLanguage);
+    setTargetLanguage(tempLanguage);
+    console.log('exchange');
   };
   return (
     <>
@@ -61,7 +91,10 @@ const Translator = () => {
             rows={4}
             className='form-control'
             value={inputText}
-            onChange={(e) => setInputText(e.target.value)}
+            onChange={(e) => {
+              setInputText(e.target.value);
+              optimizedFn(e.target.value, targetLanguage);
+            }}
             placeholder='Enter your text here'
           />
           <div className='icons'>
@@ -69,17 +102,20 @@ const Translator = () => {
               onClick={() => navigator.clipboard.writeText(inputText)}
               className='bi bi-clipboard'
             ></i>
-            <i className='bi bi-volume-up'></i>
+            <i onClick={() => sound(inputText)} className='bi bi-volume-up'></i>
             <i onClick={() => setInputText('')} className='bi bi-x-lg'></i>
           </div>
         </div>
-        <div>
+        <div onClick={exchange}>
           <i className='bi bi-arrow-left-right'></i>
         </div>
         <div className='sub-container'>
           <select
             className='form-select form-select-sm'
-            onChange={(e) => setTargetLanguage(e.target.value)}
+            onChange={(e) => {
+              setTargetLanguage(e.target.value);
+              optimizedFn(inputText, e.target.value);
+            }}
             value={targetLanguage}
           >
             {languages.map((language) => (
@@ -100,14 +136,14 @@ const Translator = () => {
               onClick={() => navigator.clipboard.writeText(translatedText)}
               className='bi bi-clipboard'
             ></i>
-            <i className='bi bi-volume-up'></i>
+            <i
+              onClick={() => sound(translatedText)}
+              className='bi bi-volume-up'
+            ></i>
             <i onClick={() => setTranslatedText('')} className='bi bi-x-lg'></i>
           </div>
         </div>
       </div>
-      <button type='submit' onClick={translate}>
-        Translate
-      </button>
     </>
   );
 };
--- src/googleTranslate.js
+++
@@ -1,3 +0,0 @@
-const apiKey = process.env.GOOGLE_TRANSLATE_API_KEY;
-
-// export const googleTranslate = require("google-translate")(apiKey);
